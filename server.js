e7f722a357feda11818f0551ba02d030efbcf649
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-03-11 18:32:06 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,126 @@
+var config = require('./config.js');
+
+var fs = require('fs');
+var dns = require('native-dns');
+
+
+var cache = require('memory-cache');
+
+// load hosts into cache
+if (config.usehosts) {
+    fs.readFile('./custom/hosts', {encoding: 'utf8'}, function(err, data) {
+        var hosts = data.split("\n");
+        hosts.forEach(function(host) {
+            if (!host.startsWith("#") && host !== '') {
+                host = host.replace(/\s+|\t/g, '|').split('|');
+                cache.put(host[1], {type: 'A', data: host[0]});
+            }
+        });
+    });
+}
+
+// load blacklist into memory
+var blacklist = [];
+if (config.useblacklist) {
+    fs.readFile('./custom/blacklist', {encoding: 'utf8'}, function(err, data) {
+        var blist = data.split("\n");
+        blist.forEach(function(ip) {
+            if (!ip.startsWith("#") && ip !== '') {
+                blacklist.push(ip);
+            }
+        });
+    });
+}
+
+// functions
+
+function dnsserv(request, response) {
+    var name = request.question[0].name,
+        type = dns.consts.qtypeToName(request.question[0].type),
+        sourceIP = request.address.address;
+
+
+}
+
+function exitError(err) {
+    console.log('Error detected:\n' + err + '\nWill now exit.');
+    process.exit(1);
+}
+// http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str){
+        return this.slice(0, str.length) == str;
+    };
+}
+if (typeof String.prototype.endsWith != 'function') {
+    String.prototype.endsWith = function (str){
+        return this.slice(-str.length) == str;
+    };
+}
+
+
+
+// process
+
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+
+if (cluster.isMaster) {
+    console.log("Starting master process...");
+
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    /*
+     cluster.on('listening', function(worker, address){
+     console.log('listening: worker ' + worker.process.pid + ', Address: ' + address.address + ":" + address.port);
+     });
+
+     cluster.on('exit', function(worker, code, signal) {
+     console.log('worker ' + worker.process.pid + ' exited.');
+     });*/
+} else {
+
+// Start servers
+    var UDPserver = dns.createServer({ dgram_type: 'udp4' });
+    UDPserver.serve(config.port);
+
+// TCP server
+    if (config.enableTCP) {
+        var TCPserver = dns.createTCPServer();
+        if (config.enableV6) {
+            TCPserver.serve(config.port, '::');
+        } else {
+            TCPserver.serve(config.port);
+        }
+    }
+
+// IPv6
+    if (config.enableV6) {
+        var UDPserver6 = dns.createUDPServer({ dgram_type: 'udp6' });
+        UDPserver6.serve(config.port);
+    }
+
+    console.log('DNS Server started at port ' + config.port + '.');
+
+// Query events...
+    UDPserver.on('request', dnsserv);
+    UDPserver6.on('request', dnsserv);
+    TCPserver.on('request', dnsserv);
+
+    UDPserver.on('error', function (err, buff, req, res) {
+        console.log('UDP Server ERR:\n');
+        console.log(err);
+    });
+    UDPserver6.on('error', function(err, buff, req, res) {
+        console.log('UDP6 Server ERR:\n');
+        console.log(err);
+    });
+    TCPserver.on('error', function (err, buff, req, res) {
+        console.log('TCP Server ERR:\n');
+        console.log(err);
+    });
+
+}
